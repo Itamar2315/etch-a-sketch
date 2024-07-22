58b9815839fc99956fb7def821c0490014cd8cb2
itamar2315 <itamar2315@gmail.com>
2024-07-22 22:54:41 +0300

    change grid to 16x16 and make it change color when mouse hover on it

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const homeDivs = [];
 const containerDiv = document.querySelector(".container");
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 256; i++){
     const newDiv = document.createElement("div");
     newDiv.classList.add('homeDivs');
     newDiv.textContent = "Boy number " + i;
+    newDiv.addEventListener("mouseover", (event) =>{
+        event.target.style.backgroundColor = "white";
+    });
+    newDiv.addEventListener("mouseleave", (event) => {
+        event.target.style.backgroundColor = "red";
+    });
+
     containerDiv.appendChild(newDiv);
+}
+
+
+function hoverDiv(){
+    
 }
