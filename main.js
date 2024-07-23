de39dccf0017d307e5ebb8c353dddb888350d79c
itamar2315 <itamar2315@gmail.com>
2024-07-23 12:57:38 +0300

    add restart button with customizable grid size, divs change to random color after hovering it!

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
 const homeDivs = [];
 const containerDiv = document.querySelector(".container");
-for(let i = 0; i < 256; i++){
-    const newDiv = document.createElement("div");
-    newDiv.classList.add('homeDivs');
-    newDiv.textContent = "Boy number " + i;
-    newDiv.addEventListener("mouseover", (event) =>{
-        event.target.style.backgroundColor = "white";
-    });
-    newDiv.addEventListener("mouseleave", (event) => {
-        event.target.style.backgroundColor = "red";
-    });
+createGrid(16);
 
-    containerDiv.appendChild(newDiv);
+const restartBtn = document.querySelector("#restartBtn");
+restartBtn.addEventListener("click", () =>{
+    let gridProportions = prompt("Enter square grid proportion!")
+    while(gridProportions < 1 || gridProportions > 100){
+        gridProportions = prompt("Enter a valid square grid proportion! (1-100)")
+    }
+    createGrid(gridProportions);
+});
+
+function createGrid(proportionNum){
+    const amount = proportionNum * proportionNum;
+    const newGridDivs = [];
+    const newDivSize = containerDiv.offsetWidth / proportionNum;
+    for(let i = 0; i < amount; i++){
+        const newDiv = document.createElement("div");
+        newDiv.style.width = newDivSize + "px";
+        newDiv.style.height = newDivSize + "px";
+
+        
+        newDiv.addEventListener("mouseover", (event) =>{
+            event.target.style.backgroundColor = "black";
+        });
+        newDiv.addEventListener("mouseleave", (event) => {
+            const random = Math.random();
+            const randomColor = Math.round((0xffffff * random)).toString(16);
+            event.target.style.backgroundColor = "#" + randomColor;
+            
+        });
+        newGridDivs.push(newDiv);
+    containerDiv.replaceChildren(...newGridDivs);
+    }
 }
 
-
-function hoverDiv(){
-    
-}
