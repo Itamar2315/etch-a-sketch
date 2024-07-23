c76299358adecd7cade67e0955b142681735ee7e
itamar2315 <itamar2315@gmail.com>
2024-07-23 13:56:50 +0300

    each square gets 1 random color once, then each hover its opacity increase

--- main.js
+++ main.js
@@ -11,27 +11,39 @@ restartBtn.addEventListener("click", () =>{
     createGrid(gridProportions);
 });
 
-function createGrid(proportionNum){
-    const amount = proportionNum * proportionNum;
+function createGrid(edgeLength){
+    const squareNum = edgeLength * edgeLength;
     const newGridDivs = [];
-    const newDivSize = containerDiv.offsetWidth / proportionNum;
-    for(let i = 0; i < amount; i++){
+    const newDivSize = containerDiv.offsetWidth / edgeLength;
+    for(let i = 0; i < squareNum; i++){
         const newDiv = document.createElement("div");
         newDiv.style.width = newDivSize + "px";
         newDiv.style.height = newDivSize + "px";
 
         
         newDiv.addEventListener("mouseover", (event) =>{
-            event.target.style.backgroundColor = "black";
-        });
-        newDiv.addEventListener("mouseleave", (event) => {
-            const random = Math.random();
-            const randomColor = Math.round((0xffffff * random)).toString(16);
-            event.target.style.backgroundColor = "#" + randomColor;
-            
+            if(event.target.style.backgroundColor){
+                const currentOpacity = Number(event.target.style.opacity);
+                if(currentOpacity < 1){
+                    event.target.setAttribute("opacity", currentOpacity + 0.1);
+                    event.target.style.opacity = currentOpacity + 0.1;
+                }
+            }
+            else{
+                randomDivColor(event);
+            }
         });
         newGridDivs.push(newDiv);
     containerDiv.replaceChildren(...newGridDivs);
     }
 }
 
+function randomDivColor(event){
+    const randomRed = Math.random() * 255;
+    const randomGreen = Math.random() * 255;
+    const randomBlue = Math.random() * 255;
+    event.target.style.backgroundColor = 
+    "rgb(" + randomRed + "," + randomGreen + ","
+    + randomBlue +")";
+    event.target.style.opacity = 0.1;
+}
